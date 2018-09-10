88df1741ac3191d03e024aabf428512516ec38d1
Ben Newman <benjamin-newman@live.co.uk>
2018-09-10 15:48:37 +0300

    ammended according to Marios comments > #7

--- server/database/dbconnection.js
+++ server/database/dbconnection.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 require("env2")("./config.env");
 
 let { DEV_DB_URL } = process.env;
-// if (process.env.NODE_ENV === 'test') {
-//   DB_URL = process.env.TEST_DB_URL;
-// }
+if (process.env.NODE_ENV === "test") {
+  DB_URL = process.env.TEST_DB_URL;
+}
 
 if (!DEV_DB_URL) throw new Error("Enviroment variable DEV_DB_URL must be set");
 
