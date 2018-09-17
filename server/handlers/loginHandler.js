796c7d8b53ac3cb5ffc7d75959cf3ebf2b8876cb
Ben Newman <benjamin-newman@live.co.uk>
2018-09-17 23:24:00 +0300

    cleaned up the coe >#23

--- server/handlers/loginHandler.js
+++ server/handlers/loginHandler.js
@@ -1,28 +1,33 @@
 const bcrypt = require('bcrypt');
 const { sign, verify } = require('jsonwebtoken');
-
 const { getPassword } = require('../queries/getData');
 const { postLastLogin } = require('../queries/postData');
 
+const { SECRET } = process.env;
+
 module.exports = (req, res) => {
-  getPassword(req.body.username).then((passwordInDatabase) => {
-    if (passwordInDatabase.length === 0) {
-      res.json({ status: 'Username does not exist' });
-      return;
-    }
-    bcrypt
-      .compare(req.body.password, passwordInDatabase[0].password)
-      .then((result) => {
-        if (result === true) {
-          postLastLogin(req.body.username).then(() => console.log);
-          res.json({ status: 'login success' });
-        } else {
-          res.json({ status: 'Incorrect password' });
-        }
-      })
-      .catch((err) => {
-        getPassword(req.body.username).then(password => console.log(password));
-        res.json({ status: 'Unknown server error. Try again later' });
-      });
-  });
+  getPassword(req.body.username)
+    .then((passwordInDatabase) => {
+      if (passwordInDatabase.length === 0) {
+        res.json({ status: 'Username does not exist' });
+        return;
+      }
+      bcrypt
+        .compare(req.body.password, passwordInDatabase[0].password)
+        .then((result) => {
+          if (result === true) {
+            const cookie = sign(req.body.username, SECRET);
+            res.json({ status: 'login success', cookie });
+            res.end();
+          } else {
+            res.json({ status: 'Incorrect password' });
+          }
+        })
+        .catch(() => {
+          res.json({ status: 'Unknown server error. Try again later' });
+        });
+    })
+    .catch(() => {
+      res.json({ status: 'Unknown server error. Try again later' });
+    });
 };
