c0e579e7b1157cf4d858acf8deb7bf04f6996be3
Ben Newman <benjamin-newman@live.co.uk>
2018-09-18 23:15:09 +0300

    404 page updated > #48

---
+++ client/src/pages/NotFoundPage/NotFoundPage.style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Div404 = styled.div`
+  background-color: #2ed573;
+  width: 100%;
+  height: 1000px;
+`;
+
+export const Header404 = styled.header`
+  display: flex;
+  vertical-align: middle;
+  justify-content: center;
+  font-size: 400px;
+`;
+
+export const Small404 = styled.header`
+  display: flex;
+  vertical-align: middle;
+  justify-content: center;
+  font-size: 40px;
+`;
