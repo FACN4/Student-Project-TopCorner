b51a4fcc3d7a9a075bf69200b992d859b5e8ebc3
Ben Newman <benjamin-newman@live.co.uk>
2018-09-20 10:56:55 +0300

    tiny change

--- client/src/components/WhiteBar/WhiteBar.style.js
+++ client/src/components/WhiteBar/WhiteBar.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const WhiteDiv = styled.div`
   position: fixed;
   top: 100px;
-  width: 242px;
+  width: 201.5px;
   height: 40px;
   background-color: #ede9e9;
   z-index: 1;
